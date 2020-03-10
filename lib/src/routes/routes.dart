12779051372cbab8197ae4b761b9aff950b92596
Fer <fer.ortego1988@gmail.com>
2020-03-10 14:52:41 +0100

    Animated Container

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 
 import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
@@ -15,6 +16,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
         '/'     : (BuildContext context) => HomePage(),
         'alert' : (BuildContext context) => AlertPage(),
         'avatar': (BuildContext context) => AvatarPage(),
-        'card'  : (BuildContext context) => CardPage()
+        'card'  : (BuildContext context) => CardPage(),
+        'animatedContainer'  : (BuildContext context) => AnimatedContainerPage()
       };
 }  
