e46ec751f1a724b824629bd29282f387c879dca2
Fer <fer.ortego1988@gmail.com>
2020-03-10 17:17:43 +0100

    Datepicker with idiom change to spanish

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,38 @@
-
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 //import 'package:componentes/src/pages/home_temp.dart';
 
-
- 
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'), // English
+        const Locale('es', 'ES'), // Spanish
+        const Locale.fromSubtags(
+            languageCode: 'zh'), // Chinese *See Advanced Locales below*
+        // ... other locales the app supports
+      ],
       // home: HomePage(),
       initialRoute: '/',
       routes: getApplicationRoutes(),
-      onGenerateRoute: ( RouteSettings settings ) {
-
+      onGenerateRoute: (RouteSettings settings) {
         return MaterialPageRoute(
-          builder: (BuildContext context) => AlertPage()
-        );
-
+            builder: (BuildContext context) => AlertPage());
       },
-      
     );
   }
 }
-
--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -8,7 +8,9 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   String _nombre = '';
   String _email = '';
-  String _password ='';
+  String _fecha = '';
+
+  TextEditingController _inputFieldDateControler = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +24,8 @@ class _InputPageState extends State<InputPage> {
             _crearEmail(),
             Divider(),
             _crearPassword(),
+            Divider(),
+            _crearFecha(context),
             Divider(),
             _crearPersona(),
  
@@ -79,8 +83,42 @@ class _InputPageState extends State<InputPage> {
           suffixIcon: Icon(Icons.lock_open),
           icon: Icon(Icons.lock)),
       onChanged: (valor) => setState((){
-        _password = valor;
+
       })
     );
+  }
+  Widget _crearFecha(BuildContext context) {
+    return TextField(
+      enableInteractiveSelection: false,
+      controller: _inputFieldDateControler,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          hintText: 'Fecha de nacimiento',
+          labelText: 'Fecha de nacimiento',
+          suffixIcon: Icon(Icons.perm_contact_calendar),
+          icon: Icon(Icons.calendar_today)),
+          onTap: () {
+            FocusScope.of(context).requestFocus(new FocusNode());
+            _selectDate(context);
+          },
+    );
+  }
+
+  _selectDate(BuildContext context) async {
+    
+    DateTime picked = await showDatePicker(
+      context: context, 
+      initialDate: new DateTime.now(), 
+      firstDate: new DateTime(2018), 
+      lastDate: new DateTime(2025),
+      locale: Locale('es', 'ES')
+      );
+    
+    if (picked != null) {
+      setState(() {
+        _fecha = picked.toString();
+        _inputFieldDateControler.text = _fecha;
+      });
+    }
   }
 }
