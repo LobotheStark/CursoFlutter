988bc2772c81ac776617b27d4ba342c451b61686
Fer <fer.ortego1988@gmail.com>
2020-03-11 18:09:56 +0100

    Checkbox and Switches

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -6,50 +6,69 @@ class SliderPage extends StatefulWidget {
 }
 
 class _SliderPageState extends State<SliderPage> {
-
   double _valorSlider = 100.0;
+  bool _bloquearCheck = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Slider')
-      ),
+      appBar: AppBar(title: Text('Slider')),
       body: Container(
-        padding: EdgeInsets.only(top:50.0),
+        padding: EdgeInsets.only(top: 50.0),
         child: Column(
           children: <Widget>[
             _crearSlider(),
-            Expanded(
-              child: _crearImagen()
-            ),
+            _crearCheckBox(),
+            _crearSwitch(),
+            Expanded(child: _crearImagen()),
           ],
         ),
       ),
-      
     );
   }
 
   Widget _crearSlider() {
-
     return Slider(
-      activeColor: Colors.indigo,
-      label: 'Tamaño de la imagen',
-      value: _valorSlider,
-      min: 10.0,
-      max: 400.0,
-      onChanged: (valor) {
-        setState(() => _valorSlider = valor);
-        
-      }
-      );
+        activeColor: Colors.indigo,
+        label: 'Tamaño de la imagen',
+        value: _valorSlider,
+        min: 10.0,
+        max: 400.0,
+        onChanged: (_bloquearCheck) ? null : (valor) {
+                setState(() => _valorSlider = valor);
+              });
   }
 
   Widget _crearImagen() {
     return Image(
-      image: NetworkImage('https://i.pinimg.com/originals/f5/9f/aa/f59faa929c29401ffa67a41475f23330.png'),
+      image: NetworkImage(
+          'https://i.pinimg.com/originals/f5/9f/aa/f59faa929c29401ffa67a41475f23330.png'),
       width: _valorSlider,
       fit: BoxFit.contain,
     );
+  }
+
+  Widget _crearCheckBox() {
+    // return Checkbox(
+    //     value: _bloquearCheck,
+    //     onChanged:(valor) {
+    //             setState(() => _bloquearCheck = valor);
+    //           });
+
+    return CheckboxListTile(
+        title: Text('Bloquear slider'),
+        value: _bloquearCheck,
+        onChanged: (valor) {
+          setState(() => _bloquearCheck = valor);
+        });
+  }
+
+  Widget _crearSwitch() {
+    return SwitchListTile(
+        title: Text('Bloquear slider'),
+        value: _bloquearCheck,
+        onChanged: (valor) {
+          setState(() => _bloquearCheck = valor);
+        });
   }
 }
